422d6c82c0088d0d7118c604242f694d323d8d41
melovitor <vitormelobatista@hotmail.com>
2024-01-11 17:31:34 -0300

    Tabela de transação

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,31 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
     return (
         <div>
             <Header/>
             <Summary/>
+        
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width='40%'>Desenvolvimento de site</td>
+                            <td><PriceHighLight variant="income">R$10.00</PriceHighLight></td>
+                            <td>venda</td>
+                            <td>11/01/2024</td>
+                        </tr>
+                        <tr>
+                            <td width='40%'>Alimentação</td>
+                            <td><PriceHighLight variant="outcome">-R$10.00</PriceHighLight></td>
+                            <td>venda</td>
+                            <td>11/01/2024</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
